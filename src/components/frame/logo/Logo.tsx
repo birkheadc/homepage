c653d156c4d1ad19dd5b10871ef711cbe596905f
Colby Birkhead <birkheadc@gmail.com>
2024-01-30 21:14:39 +0900

    fix font on short screens in logo

--- src/components/frame/logo/Logo.tsx
+++ src/components/frame/logo/Logo.tsx
@@ -9,7 +9,7 @@ interface LogoProps {
 export default function Logo(props: LogoProps): JSX.Element {
   return (
     <div className='relative flex items-center justify-center w-full h-full overflow-hidden '>
-      <h1 className={utils.cn(styles.text, 'text-3xl lg:text-5xl uppercase font-bold text-black')}>game guy</h1>
+      <h1 className={utils.cn(styles.text, 'text-3xl lg:text-5xl short:text-2xl uppercase font-bold text-black')}>game guy</h1>
     </div>
   );
 }
