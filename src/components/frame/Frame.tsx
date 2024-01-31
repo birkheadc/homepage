4438edadc58988a12669d7301bb0e4ed46b6d165
Colby Birkhead <birkheadc@gmail.com>
2024-01-31 17:19:07 +0900

    fix bug in frame-skin not being changeable; remove infinite loop from logo animation

--- src/components/frame/Frame.tsx
+++ src/components/frame/Frame.tsx
@@ -17,12 +17,23 @@ type FrameProps = {
 export default function Frame(props: FrameProps): JSX.Element {
 
   const { zoomIn, zoomOut, isZoomedIn } = React.useContext(FrameContext);
-  const { skin, skinStyle, changeSkin } = useSkin();
+  const { skin, changeSkin } = useSkin();
   const { theme } = useTheme();
 
   const toggleZoom = () => {
     isZoomedIn ? zoomOut() : zoomIn();
   }
+
+  const SKIN_STYLES: { [key: string]: string } = {
+    neutral: 'from-neutral-300 to-neutral-500 border-neutral-900 border-t-neutral-600 border-l-neutral-600',
+    berry: 'from-berry-300 to-berry-500 border-berry-900 border-t-berry-600 border-l-berry-600',
+    grape: 'from-grape-300 to-grape-500 border-grape-900 border-t-grape-600 border-l-grape-600',
+    kiwi: 'from-kiwi-300 to-kiwi-500 border-kiwi-900 border-t-kiwi-600 border-l-kiwi-600',
+    dandelion: 'from-dandelion-300 to-dandelion-500 border-dandelion-900 border-t-dandelion-600 border-l-dandelion-600',
+    teal: 'from-teal-300 to-teal-500 border-teal-900 border-t-teal-600 border-l-teal-600'
+  }
+
+  const skinStyle = SKIN_STYLES[skin];
 
   const CLASS_NAMES = {
     common: {
