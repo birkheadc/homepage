5f5481e18e7a522febcaa7ce96065d9921911bca
Colby Birkhead <birkheadc@gmail.com>
2024-02-04 18:36:52 +0900

    get translations 'working'

--- src/app/[locale]/sections/introduction/IntroductionSection.tsx
+++ src/app/[locale]/sections/introduction/IntroductionSection.tsx
@@ -14,9 +14,15 @@ export default function IntroductionSection(props: IntroductionSectionProps): JS
   return (
     <div className='relative w-full h-full'>
       <div className='flex flex-col items-end justify-start p-6'>
-        <h2 className='text-5xl lg:text-7xl'>{t('header.thisIs')} <span className='text-primary-0'>{t('header.colby')}</span></h2>
-        <span className='text-2xl lg:text-4xl'>{t('span-1')}</span>
-        <span className='text-lg lg:text-2xl'>{t('span-2.onThe')} <span className='text-primary-0'>{t('span-2.internet')}</span> {t('span-2.ofAllThings')}</span>
+        <h2 className='text-5xl lg:text-7xl'>
+          {t.rich('header', {
+            em: (value) => {
+              return <span className='text-primary-0'>{value}</span>
+            }
+          })}
+        </h2>
+        <span className='text-2xl lg:text-4xl'></span>
+        <span className='text-lg lg:text-2xl'></span>
       </div>
       <PixelatedImage className='absolute bottom-0 max-w-full max-h-full ' img={'/colby/400.png'} shaderMode={ImageProcessShaderMode.NORMAL} pixelLevel={1} />
     </div>
