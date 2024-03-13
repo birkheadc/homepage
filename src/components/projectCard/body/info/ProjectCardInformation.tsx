6014808ec9680d9254c52522d050a1f6e1fa6662
Colby Birkhead <birkheadc@gmail.com>
2024-03-13 16:12:29 +0900

    fix project card information not extending to fill width

--- src/components/projectCard/body/info/ProjectCardInformation.tsx
+++ src/components/projectCard/body/info/ProjectCardInformation.tsx
@@ -18,7 +18,7 @@ export default function ProjectCardInformation(props: ProjectCardInformationProp
   return (
     <div className={utils.cn('absolute inset-0 z-10 flex flex-col items-center justify-between transition-all bg-opacity-75 opacity-0 hocus-within:opacity-100 backdrop-blur-sm bg-primary-3', styles.information)}>
       <ProjectTechnologiesList className={styles.technologies} technologies={project.technologies} />
-      <div className='flex-grow px-2 border-l-4 border-r-4 border-b-4 border-primary-2'>
+      <div className='flex-grow px-2 border-l-4 border-r-4 border-b-4 border-primary-2 w-full'>
         <p className={utils.cn('lg:text-lg whitespace-pre-wrap text-pretty', styles.text)}>{project.descriptions.shortDescriptions.find(d => d.language === language)?.content}</p>
       </div>
       <ProjectCardLinks project={project} styles={styles} showReadMore />
