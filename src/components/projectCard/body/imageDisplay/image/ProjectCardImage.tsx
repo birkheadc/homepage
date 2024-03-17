2fee9741e6819f00f197b2133f138b0b01f8ad00
Colby Birkhead <birkheadc@gmail.com>
2024-03-17 17:13:55 +0900

    fix project card image not panning on first interval... hopefully

--- src/components/projectCard/body/imageDisplay/image/ProjectCardImage.tsx
+++ src/components/projectCard/body/imageDisplay/image/ProjectCardImage.tsx
@@ -3,6 +3,7 @@ import PixelatedImage from '../../../../pixelatedImage/PixelatedImage';
 import { ImageProcessShaderMode } from '../../../../../types/image/imageProcessShaderMode';
 import utils from '../../../../../utils';
 import styles from './ProjectCardImage.module.css';
+import Spinner from '../../../../spinner/Spinner';
 
 type ProjectCardImageProps = {
   outerDivSize: { width: number, height: number },
@@ -14,6 +15,13 @@ export default function ProjectCardImage(props: ProjectCardImageProps): JSX.Elem
 
   const { outerDivSize, image, isCurrent } = props;
   const wrapperRef = React.useRef<HTMLDivElement>(null);
+
+  const [ isReady, setReady ] = React.useState<boolean>(false);
+
+  const handleReady = () => {
+    setReady(true);
+    init();
+  }
 
   const init = () => {
     if (wrapperRef.current == null) return;
@@ -32,8 +40,11 @@ export default function ProjectCardImage(props: ProjectCardImageProps): JSX.Elem
   }, [ wrapperRef, outerDivSize, init ]);
 
   return (
-    <div className={utils.cn('bg-primary-0 absolute top-0 left-0', isCurrent ? styles.image : 'hidden')} ref={wrapperRef}>
-      <PixelatedImage onReady={init} img={image} shaderMode={ImageProcessShaderMode.NORMAL} pixelLevel={1} />
-    </div>
+    <>
+      { !isReady && <Spinner />}
+      <div className={utils.cn('bg-primary-0 absolute top-0 left-0', isCurrent ? styles.image : 'hidden')} ref={wrapperRef}>
+        <PixelatedImage onReady={handleReady} img={image} shaderMode={ImageProcessShaderMode.NORMAL} pixelLevel={1} />
+      </div>
+    </>
   );
 }
